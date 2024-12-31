de43022cb09eafede6417e0b12e7d0ccc381c190
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-12-31 23:09:27 +0530

    changes in axios

--- src/config/axiosInstance.jsx
+++ src/config/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://ramankutty.shop",
+  baseURL: "https://ramankutty.shop",
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json', 
